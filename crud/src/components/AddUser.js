9edeafa676f79d68e0e1123e74c06854aedbaf1c
tho do <thodo7199@gmail.com>
2021-03-29 09:23:20 +0700

    fix epty

--- crud/src/components/AddUser.js
+++ crud/src/components/AddUser.js
@@ -1,43 +1,48 @@
-import React,{useContext,useState} from 'react';
-import {Button, Form, FormGroup, Input, Label} from 'reactstrap'
-import {Link,useHistory} from "react-router-dom";
-import {GlobalContext} from "../context/GlobleState";
-import {v4 as uuid} from 'uuid'
+import React, { useContext, useState } from "react";
+import { Button, Form, FormGroup, Input, Label } from "reactstrap";
+import { Link, useHistory } from "react-router-dom";
+import { GlobalContext } from "../context/GlobleState";
+import { v4 as uuid } from "uuid";
 const AddUser = () => {
-    const [name,setName]=useState('')
+  const [name, setName] = useState("");
 
-    const {addUser}= useContext(GlobalContext)
-    const history=useHistory()
-    const onChange=(e)=>{
-        setName(e.target.value)
+  const { addUser } = useContext(GlobalContext);
+  const history = useHistory();
+  const onChange = (e) => {
+    setName(e.target.value);
+  };
 
+  const onSubmit = () => {
+    const newUser = {
+      id: uuid(),
+      name,
+    };
+    if (name!=='') {
+      addUser(newUser);
+      history.push("/");
     }
-
-
-    const onSubmit=()=>{
-        const  newUser={
-            id:uuid(),
-            name
-        }
-        addUser(newUser)
-        history.push('/')
-    }
-    return (
-        <div>
-          <Form onSubmit={onSubmit}>
-              <FormGroup>
-                <Label>Name</Label>
-                  <Input type="text"
-                         name="name"
-                         value={name}
-                         onChange={onChange}
-                         placeholder="Enter name"/>
-              </FormGroup>
-              <Button type='submit'>Submit</Button>
-              <Link to="/" className="btn btn-danger ml-2"> Cancel</Link>
-          </Form>
-        </div>
-    );
+  };
+  return (
+    <div>
+      <Form onSubmit={onSubmit}>
+        <FormGroup>
+          <Label>Name</Label>
+          <Input
+            type="text"
+            name="name"
+            value={name}
+            onChange={onChange}
+            placeholder="Enter name"
+          />
+        </FormGroup>
+        <Button type="submit">Submit</Button>
+        <Link to="/" className="btn btn-danger ml-2">
+          {" "}
+          Cancel
+        </Link>
+      </Form>
+    </div>
+  );
 };
 
 export default AddUser;
